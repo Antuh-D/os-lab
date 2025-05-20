ab5cb8123ce6e68562cad6469b8eb0ab4a09cb51
Antukalower <antukalower@gmail.com>
2025-05-21 00:09:04 +0600

    Added test shell

---
+++ Shell/test.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+echo "This is test"
+
+echo Our shell name is  $BASH
+echo Our shell version is $BASH_VERSION
+echo  Our home directory is $HOME
+echo Out current  working directory is $PWD
+
+name=Antu
+echo "My name is $name"
+reg=2021831052
+echo "My Registration number is $reg"
+ 
+ 
+echo "Enter your name bellow:"
+read name
+echo "Your name is: $name" 
+
+read -p "Username:" name2
